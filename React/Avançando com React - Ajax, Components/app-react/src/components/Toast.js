a6c353a6ef5a2273016b9fa9ee60c6e352be870c
Waldener Monteiro <jjrr019@gmail.com>
2020-01-24 11:38:04 -0300

    terminando curso react

---
+++ React/Avançando com React - Ajax, Components/app-react/src/components/Toast.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Toast(props){
+    const message = props
+    return (
+    <h4>{message}</h4>
+    )
+}
+export default Toast
--- React/Avançando com React - Ajax, Components/app-react/src/container/WeatherApp.js
+++ React/Avançando com React - Ajax, Components/app-react/src/container/WeatherApp.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import { getWeatherInfo } from '../API'
 function WeatherApp() {
     const [city, setCity] = useState('')
     const [weather, setWeather] = useState({})
@@ -8,15 +8,24 @@ function WeatherApp() {
         const { value } = target
         setCity(value)
     }
-    const searchApi = () => {
-        console.log('REQUEST AJAX')
+    const searchApi = async () => {
+        const { data } = await getWeatherInfo(city)
+        setWeather(data)
+        console.log('request ajax', data)
     }
     return (
         <div>
             <input type="text" name="city" placeholder="Enter City name" onChange={handleChangeInput} value={city} />
             <button type="button" onClick={searchApi}>Search</button>
             <hr></hr>
-            <div className="weather-info"></div>
+            {weather && weather.current ?
+                <div className="weather-info">
+                    <h1>{weather.current.weather_descriptions[0]}</h1>
+                    <img src={weather.current.weather_icons[0]} alt="" />
+                    <h4>{weather.current.temperature} ºC </h4>
+                </div>
+                : <p>No yet</p>
+            }
 
         </div>
     );
