91a150e593e91b3ef0ccd681fa061286cc5d7452
Waldener Monteiro <jjrr019@gmail.com>
2020-01-28 14:06:37 -0300

    concluindo react-crud

--- TestReact/app-react/src/components/Insert.js
+++ TestReact/app-react/src/components/Insert.js
@@ -2,18 +2,30 @@ import React, { useState, useEffect } from 'react';
 import { Form, Button, Label, Input, FormGroup } from 'reactstrap'
 import { Link } from 'react-router-dom'
 import { alertSuccess } from '../utils/messages'
-function Insert() {
+function Insert(props) {
+    const id = props.match.params.id || null
+    const dataInitial = localStorage.getItem("data") ? JSON.parse(localStorage.getItem('data')) : []
+
     const [name, setName] = useState('')
     const [lastName, setLastName] = useState('')
     const [date, setDate] = useState('')
     const [check, setCheck] = useState(false)
-    const [data, setData] = useState([])
+    const [data, setData] = useState(dataInitial)
     useEffect(() => {
-        setData(localStorage.getItem("data") ? JSON.parse(localStorage.getItem('data')) : [])
+        if (id) {
+            const filtered = data.filter(item => item.id === id)[0]
+            setInformations(filtered)
+        }
+    }, [data, id])
 
-    }, [])
+    const setInformations = ({ name, lastName, date, check }) => {
+        setName(name)
+        setLastName(lastName)
+        setDate(date)
+        setCheck(check)
+    }
 
-    const _generateRandomId = () => (Math.random().toString(24).substring(2,10))
+    const _generateRandomId = () => (Math.random().toString(24).substring(2, 10))
 
     const onChangeName = ({ target }) => {
         const value = target.type === 'checkbox' ? target.checked : target.value
@@ -24,7 +36,7 @@ function Insert() {
         const value = target.value
         setLastName(value)
     }
-    
+
     const onChangeDate = ({ target }) => {
         const value = target.value
         setDate(value)
@@ -34,14 +46,27 @@ function Insert() {
         setCheck(value)
     }
     const handleStore = async () => {
-        const arr = data
-        const id = _generateRandomId()
-        arr.push({ id, name, lastName, date, check })
-        await setData(arr)
-        localStorage.setItem('data', JSON.stringify(data))
-        alertSuccess('User successfully registered')
-    }
+        if (id) {
+            const filteredItem = data.filter(item => item.id === id)[0]
+            const index = data.indexOf(filteredItem)
+            let arr = data
+            arr.splice(index, 1, { id, name, lastName, date, check })
+            await setData(arr)
+            localStorage.setItem('data', JSON.stringify(data))
+            alertSuccess('User successfully modified')
+            props.history.push('/users')
 
+        } else {
+            const arr = data
+            const id = _generateRandomId()
+            arr.push({ id, name, lastName, date, check })
+            await setData(arr)
+            localStorage.setItem('data', JSON.stringify(data))
+            alertSuccess('User successfully registered')
+            props.history.push('/users')
+        }
+
+    }
     return (
         <div>
             <h2>New Item</h2>
@@ -64,7 +89,7 @@ function Insert() {
                     Done?
                 </FormGroup>
                 <FormGroup className="mt-3" >
-                    <Button color="primary" className="mr-3"  onClick={handleStore}>Add</Button>
+                    <Button color="primary" className="mr-3" onClick={handleStore}>Add</Button>
                     <Link to="/users" className="btn btn-secondary">Back To list</Link>
                 </FormGroup>
             </Form>
--- TestReact/app-react/src/utils/index.js
+++ TestReact/app-react/src/utils/index.js
@@ -1,5 +1,8 @@
 const convertDateToBr = (date) => {
-    return new Date(date).toLocaleDateString()
+    if (date === null) return ''
+    let pattern = /(\d{4})-(\d{2})-(\d{2})/g
+    let getDate = (pattern.exec(date))[0]
+    return getDate.replace(pattern, '$3/$2/$1')
 }
 export {
     convertDateToBr
