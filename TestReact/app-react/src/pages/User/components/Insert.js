f3fb09aa516e961128b9111ba755ff03883446c6
Waldener Monteiro <jjrr019@gmail.com>
2020-01-29 11:44:59 -0300

    finalizando a primeira parte do testReact

--- TestReact/app-react/src/pages/User/components/Insert.js
+++ TestReact/app-react/src/pages/User/components/Insert.js
@@ -5,71 +5,38 @@ import { alertSuccess, alertDanger } from '../../../utils/alerts'
 import UserService from '../services/UserService'
 
 function Insert(props) {
-
-
     const idRoute = props.match.params.id || null
-    const [name, setName] = useState('')
-    const [lastName, setLastName] = useState('')
-    const [date, setDate] = useState('')
-    const [check, setCheck] = useState(false)
-    const [data, setData] = useState([])
-    useEffect( async () => {
-        await get()
+    const initialUser = { id: '', name: '', lastName: '', date: '', check: false }
+    const [user, setUser] = useState(initialUser)
+    useEffect(() => {
         if (idRoute) {
-            const filtered = filteredInformation()
-            setInformations(filtered)
+            getById().then((result) => {
+                setInformations(result)
+            }).catch((error) => {
+                alertDanger(error)
+            });
         }
     }, [])
-        const get = async () => {
-        await UserService.list().then((result) => {
-            setData(result.data)
-        }).catch((err) => {
-            alertDanger(err)
-        });
-    }
-    const filteredInformation = async () => {
-        console.log(data)
-        return data.filter(item => item.id === idRoute)[0]
-    }
-    const setInformations = ({ name, lastName, date, check }) => {
-        setName(name)
-        setLastName(lastName)
-        setDate(date)
-        setCheck(check)
-    }
 
-    const _generateRandomId = () => (Math.random().toString(24).substring(2, 10))
-
-    const onChangeName = ({ target }) => {
+    const getById = async () => {
+        try {
+            const response = await UserService.show(idRoute)
+            return response
+        } catch (error) {
+            return error
+        }
+    }
+    const setInformations = (result) => {
+        setUser(result)
+    }
+ 
+    const onChange = ({ target }) => {
         const value = target.type === 'checkbox' ? target.checked : target.value
-        setName(value)
-    }
-
-    const onChangeLastName = ({ target }) => {
-        const value = target.value
-        setLastName(value)
-    }
-
-    const onChangeDate = ({ target }) => {
-        const value = target.value
-        setDate(value)
-    }
-    const onChangeCheck = ({ target }) => {
-        const value = target.type === 'checkbox' ? target.checked : target.value
-        setCheck(value)
+        setUser({ ...user, [target.name]: value })
     }
     const handleStore = async () => {
-        if (idRoute) {
-            const id = idRoute
-            await createOrUpdate({ id, name, lastName, date, check })
-            alertSuccess('User successfully modified')
-            props.history.push('/users')
-
-        } else {
-            const id = _generateRandomId()
-            await createOrUpdate({ id, name, lastName, date, check })
-            props.history.push('/users')
-        }
+        await createOrUpdate(user)
+        props.history.push('/users')
 
     }
     const createOrUpdate = async (user) => {
@@ -81,7 +48,7 @@ function Insert(props) {
             });
         } else {
             await UserService.create(user).then((result) => {
-                alertSuccess('User successfully modified')
+                alertSuccess('User successfully created')
             }).catch((err) => {
                 alertDanger(err)
             });
@@ -94,19 +61,19 @@ function Insert(props) {
             <Form>
                 <FormGroup>
                     <Label for="name">Name: </Label>
-                    <Input type="text" value={name} onChange={onChangeName} name="name" id="name" placeholder="Insert item name here" />
+                    <Input type="text" value={user.name} onChange={onChange} name="name" id="name" placeholder="Insert item name here" />
                 </FormGroup>
                 <FormGroup>
                     <Label for="lastName">LastName: </Label>
-                    <Input type="text" value={lastName} onChange={onChangeLastName} name="lastName" id="lastName" placeholder="Insert item lastName here" />
+                    <Input type="text" value={user.lastName} onChange={onChange} name="lastName" id="lastName" placeholder="Insert item lastName here" />
                 </FormGroup>
                 <FormGroup>
                     <Label for="date">Date of birth: </Label>
-                    <Input type="date" value={date} onChange={onChangeDate} name="date" id="date" placeholder="Insert item date here" />
+                    <Input type="date" value={user.date} onChange={onChange} name="date" id="date" placeholder="Insert item date here" />
                 </FormGroup>
                 <FormGroup check>
                     <Label check></Label>
-                    <Input type="checkbox" name="check" id="check" check={check} onChange={onChangeCheck} /> {' '}
+                    <Input type="checkbox" name="check" onChange={onChange} id="check" check={user.check} /> {' '}
                     Done?
                 </FormGroup>
                 <FormGroup className="mt-3" >
